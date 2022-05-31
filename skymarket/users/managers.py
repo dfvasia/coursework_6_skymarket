a40422e7f38e335eb3f10538ffce4816146e7931
dfvasia <vasilii-zhuravsk@mail.ru>
2022-05-31 06:42:22 +0300

    5

--- skymarket/users/managers.py
+++ skymarket/users/managers.py
@@ -8,8 +8,7 @@ class UserManager(BaseUserManager):
     функция создания пользователя — в нее мы передаем обязательные поля
     """
 
-    def create_user(self, email, first_name, last_name, phone, password=None, **extra_fields):
-        extra_fields.setdefault('is_active', True)
+    def create_user(self, email, first_name, last_name, phone, password=None):
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(
@@ -17,33 +16,25 @@ class UserManager(BaseUserManager):
             first_name=first_name,
             last_name=last_name,
             phone=phone,
-            role="user",
-            **extra_fields
         )
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, first_name, last_name, phone, password=None, **extra_fields):
+    def create_superuser(self, email, first_name, last_name, phone, password=None):
         """
         Функция для создания суперпользователя — с ее помощью мы создаем администратора
         это можно сделать с помощью команды createsuperuser
         """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
 
         user = self.create_user(
-            email,
+            email=self.normalize_email(email),
             first_name=first_name,
             last_name=last_name,
             phone=phone,
             password=password,
-            role="admin",
-            **extra_fields
-
         )
-
+        user.role = 'admin'
         user.save(using=self._db)
         return user
--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,3 +1,5 @@
+import enum
+
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
@@ -7,47 +9,55 @@ from django.utils.translation import gettext_lazy as _
 from users.managers import UserManager
 
 
-class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    TYPE_ADMIN = "A"
-    TYPE_USER = "U"
-    STATUS_ROLE = ((TYPE_ADMIN, "admin"), (TYPE_USER, "user"))
-
-    user_role = models.SlugField(max_length=20, unique=True, default=TYPE_USER, blank=False, null=False, choices=STATUS_ROLE)
-
-    def __str__(self):
-        return self.user_role
+class UserRoles(enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = None
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField(_('email_address'), unique=True, max_length=200)
-    date_joined = models.DateTimeField(default=timezone.now)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    role = models.ForeignKey('UserRoles', on_delete=models.PROTECT)
-    phone = PhoneNumberField()
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+    is_active = models.BooleanField(default=False, verbose_name='Статус')
+    role = models.CharField(max_length=15, blank=False, null=False, default=UserRoles.USER, verbose_name='Группа доступа')
+    phone = PhoneNumberField(verbose_name='Телефон')
+    user_permissions = models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='Разрешения')
+    groups = models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='Группы')
+    password = models.CharField(max_length=128, verbose_name='ХЕШ пароля')
+    last_login = models.DateTimeField(blank=True, null=True, verbose_name='Последний вход')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', "role"]
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
 
     objects = UserManager()
 
     @property
-    def has_perm(self, perm, obj=None):
-        """Does the user have a specific permission?"""
-        # Simplest possible answer: Yes, always
-        return True
+    def is_admin(self):
+        return self.role == UserRoles.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.USER
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
 
     @property
     def is_staff(self):
-        """Is the user a member of staff?"""
-        return self.staff
+        return self.is_admin
 
-    @property
-    def is_admin(self):
-        """Is the user a admin member?"""
-        return self.admin
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
 
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'пользователи'
